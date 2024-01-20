544eda0eb2df0a2bdd692aae723f09d28d1fe895
artindiram <35272603+artindiram@users.noreply.github.com>
2024-01-20 12:36:41 -0700

    Added Growth

--- Player/player.gd
+++ Player/player.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 32.0
+var SECTION = preload("res://Player/segment.tscn")
+
+const SPEED = 50.0
 const JUMP_VELOCITY = -400.0
+var heightCount = 0
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -22,3 +26,20 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func moveFeet(distance):
+	$FootCollision.position.y = distance * 16
+	pass
+
+func spawnSection():
+	var section = SECTION.instantiate()
+	add_child(section)
+	section.global_position = self.global_position
+	section.position.y += heightCount * 16
+	moveFeet(heightCount)
+
+func _on_growth_timer_timeout():
+	position.y -= 16
+	heightCount += 1
+	spawnSection()
+	pass # Replace with function body.
