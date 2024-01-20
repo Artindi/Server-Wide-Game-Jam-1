8d20a5400e13022865fa79421f66552e4e6a04a7
Warladd <danieljaydenjohnson@gmail.com>
2024-01-20 16:27:30 -0500

    Added optimization of code
    Title

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,18 +2,18 @@ extends Control
 @export var settings : Control
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta) -> void:
 	pass
 
-func _on_quit_button_pressed():
+func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
-func _on_play_button_pressed():
+func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Screens/temp_test.tscn")
 
-func _on_settings_button_pressed():
+func _on_settings_button_pressed() -> void:
 	settings.show()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
+@export var foot_collision : CollisionShape2D
 
 var SECTION = preload("res://scenes/Player/section.tscn")
 
 const SPEED = 32.0
 var heightCount = 0
 
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -23,19 +23,18 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-func moveFeet(distance):
-	$FootCollision.position.y = distance * 16
+func moveFeet(distance) -> void:
+	foot_collision.position.y = distance * 16
 	pass
 
-func spawnSection():
+func spawnSection() -> void:
 	var section = SECTION.instantiate()
 	add_child(section)
 	section.global_position = self.global_position
 	section.position.y += heightCount * 16
 	moveFeet(heightCount)
 
-func _on_growth_timer_timeout():
+func _on_growth_timer_timeout() -> void:
 	position.y -= 16
 	heightCount += 1
 	spawnSection()
-	pass # Replace with function body.
