09f336dec3b8ba13982b0dd439f2eb0fc849706f
Warladd <danieljaydenjohnson@gmail.com>
2024-01-20 17:49:36 -0500

    Finished
    Added level select and made the Savesystem load on open of the game.

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,17 +3,13 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	SaveSystem.load_game()
 
 func _on_quit_button_pressed():
 	get_tree().quit()
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Screens/temp_test.tscn")
+	get_tree().change_scene_to_file("res://scenes/screens/level_select.tscn")
 
 func _on_settings_button_pressed():
 	settings.show()
