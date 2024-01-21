e973fd88b30196556cf164b0fd537cba59778f3b
artindiram <35272603+artindiram@users.noreply.github.com>
2024-01-20 17:38:17 -0700

    Addded newer sprites and sprite functionality

--- scripts/section.gd
+++ scripts/section.gd
@@ -1,14 +1,28 @@
 extends CharacterBody2D
 
+var rng = RandomNumberGenerator.new
+
 var index = 0
 @onready var player = get_parent().get_parent()
 
-func _process(_delta):
+var texture1 = preload("res://assets/images/characters/Section1.png")
+var texture2 = preload("res://assets/images/characters/Section2.png")
+var texture3 = preload("res://assets/images/characters/Section3.png")
+
+func _ready():
+	randomize()
+	var texture = randi_range(1, 3)
+	if texture == 1:
+		$Sprite2D.texture = texture1
+	elif texture == 2:
+		$Sprite2D.texture = texture2
+	elif texture == 3:
+		$Sprite2D.texture = texture3
+
+func _physics_process(delta):
 	if position.y >= player.get_node("FootCollision").position.y:
 		queue_free()
 	pass
-
-
 
 func _on_area_2d_body_entered(body):
 	if body.name == "TileMap":
