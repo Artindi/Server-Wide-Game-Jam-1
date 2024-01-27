da380cfc2f3e8b845f89040b00bba07ccd8b13c0
NexusKitten <127152751+NexusKitten@users.noreply.github.com>
2024-01-27 18:46:52 -0500

    Add levels 8, 9

--- scripts/Collectable.gd
+++ scripts/Collectable.gd
@@ -1,7 +1,5 @@
 extends Area2D
 
-var collected = false
-
 func _ready():
 	$Sprite2D.play("default")
 	if SaveSystem.save_game.collection.has(get_parent().get_meta("flag")):
@@ -14,7 +12,4 @@ func _on_body_entered(body):
 	if !SaveSystem.save_game.collection.has(get_parent().get_meta("flag")):
 		SaveSystem.save_game.collection.append(get_parent().get_meta("flag"))
 	$Sprite2D.play("collected")
-	if collected == false:
-		collected = true
-		$CollectedSound.play()
 	pass
