a33322f7f1c872e49935d69c7044a7dd4bc95a9a
Warlordthe3rd <danieljaydenjohnson@gmail.com>
2024-01-30 19:29:54 -0500

    Stuff
    Changed level 12 to not have soil, fixed a bug with level select when you had level 15 unlocked, added credits, made an option to disable player controls. Still need to finish the credits, there is a weird bug where the character keeps breaking.

--- scripts/levels.gd
+++ scripts/levels.gd
@@ -3,6 +3,12 @@ extends Node2D
 
 func _ready() -> void:
 	SaveSystem.load_game()
+		
+func _physics_process(_delta) -> void:
+	if !MusicController.get_node("Music").is_playing():
+		check_music()
+
+func check_music():
 	if objective.next_level <= 6:
 		if !MusicController.get_node("Music").is_playing():
 			MusicController.play_music("res://assets/soundFX/nayslayer.mp3")
@@ -11,8 +17,11 @@ func _ready() -> void:
 			MusicController.get_node("Music").stop()
 		if !MusicController.get_node("Music").is_playing():
 			MusicController.play_music("res://assets/soundFX/godray.mp3")
-	elif objective.next_level <= 16:
+	elif objective.next_level <= 15:
 		if objective.next_level == 12:
 			MusicController.get_node("Music").stop()
 		if !MusicController.get_node("Music").is_playing():
 			MusicController.play_music("res://assets/soundFX/penguinbeep.mp3")
+	elif objective.next_level == 16:
+		MusicController.get_node("Music").stop()
+		MusicController.play_music("res://assets/soundFX/penguinbeep.mp3")
