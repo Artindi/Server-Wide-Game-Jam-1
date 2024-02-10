dbbe83d3250642b30b4b418248dfcfe25f714bdf
artindiram <35272603+artindiram@users.noreply.github.com>
2024-02-10 09:26:16 -0700

    Small fixes before release

--- scripts/SaveSystem.gd
+++ scripts/SaveSystem.gd
@@ -10,6 +10,7 @@ func saving() -> void:
 		err = ResourceSaver.save(save_game, "user://savegame.tres")
 	if err != OK:
 		print("Save Game Error: ", err)
+	pass
 
 func load_game() -> void:
 	if !FileAccess.file_exists("user://savegame.tres") and !FileAccess.file_exists("user://savegame.res"):
@@ -24,7 +25,9 @@ func load_game() -> void:
 	SaveSystem.save_game.update_vol(0, SaveSystem.save_game.master_vol_value)
 	SaveSystem.save_game.update_vol(1, SaveSystem.save_game.music_vol_value)
 	SaveSystem.save_game.update_vol(2, SaveSystem.save_game.sfx_vol_value)
+	pass
 
 func restart() -> void:
 	save_game = SaveFile.new()
 	saving()
+	pass
